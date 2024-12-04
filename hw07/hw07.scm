73a059441397fe4256b14e912cd096be897eb7e4
Haoqing Wang <1506751656@qq.com>
2024-12-04 16:38:13 +0800

    finish hw07

--- hw07/hw07.scm
+++ hw07/hw07.scm
@@ -1,15 +1,19 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+(cond ((= 0 exp) 1)
+  ((odd? exp) (* base (pow base  (- exp 1) )))
+  (else (pow (square base) (/ exp 2)))
+  ))
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let ((y (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car(cddr s)))
